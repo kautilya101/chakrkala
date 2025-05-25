28d902cc042a0631cc8e018fe6632f9ff5bc4c3e
kautilya101 <kautilya101001@gmail.com>
2025-05-26 00:19:39 +0530

    color updated

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <Header />
         <main className='flex-grow flex items-center justify-center text-wrap text-center '>
           <span className='flex flex-col items-center justify-center gap-6'>    
-            <h2 className='text-5xl md:text-8xl text-center opacity-70'>
+            <h2 className='text-5xl md:text-8xl text-center'>
               Fill Your Walls With
             </h2>
             <h1 className='text-7xl md:text-9xl text-center opacity-70'>
