8e89845b32becc5332f38e573211d963da55123a
kautilya101 <kautilya101001@gmail.com>
2025-06-01 20:21:30 +0530

    contact details updated

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -24,13 +24,13 @@ const Contact = () => {
       <div className=" h-[60vh] md:h-[80vh] px-10 md:px-16 flex flex-col justify-center gap-6 md:gap-20" id="details">
         <div className="flex flex-col gap-4">
           <h2 className="text-2xl sm:text-5xl font-bold text-[var(--primary)]">Get in Touch</h2>
-          <a href="mailto:example@email.com" className="text-[var(--secondary)] underline text-base md:text-2xl">chakrkala@gmail.com</a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=goelmus98@gmail.com" className="text-[var(--secondary)] underline text-base md:text-2xl">hello@chakrakala.com</a>
         </div>
 
         <div className="flex flex-col gap-6">
           <h3 className="text-xl sm:text-5xl font-semibold mt-8 text-[var(--primary)]">Reach out & Order</h3>
           <a
-            href="https://www.instagram.com/100_shades_of_mandala/"
+            href="https://www.instagram.com/hundred_shades_of_mandala"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2 text-[var(--secondary)] text-base md:text-2xl hover:underline"
